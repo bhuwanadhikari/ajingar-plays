37d7c99b96982df2929ca0e99bee3faeb8b714ae
bhuwanadhikari <adhikaribhuwan@gmail.com>
2020-02-17 19:54:03 +0545

    Add cloud-jump

--- cloudJump.py
+++ cloudJump.py
@@ -1,22 +1,23 @@
 def jumpingOnClouds(c):
-    step = 0
-    count = 0
-    for i in range(len(c)):
-        if i>= count and len(c)-3>= i :
-            if c[i+1] == 1:
-                count = count+2
-            if c[i+1] == 0 and c[ i + 2] == 0:
-                count = count+2
-            if c[i+1] == 0 and c[i+2] == 1:
-                count = count +1
-            step += 1
-        elif len(c)-1:
-            step += 1
+    steps = 0
+    x = 0
+    while(x<len(c)-2):
+        if c[x+1] == 0 and c[x+2] == 0:
+            x += 2
+        elif c[x+1] == 0 and c[x+2] == 1:
+            x += 1
+        elif c[x+1] == 1 and c[x+2] == 0:
+            x += 2
+        else:
+            x =+1
+        
+        steps = steps+1
+        
+    if c[-1] == 0 and x == len(c)-2: steps += 1
+    print(steps)
+    return steps
 
-    print(step)
-    return step
 
 
 
-
-jumpingOnClouds([0, 0, 0, 0, 1, 0])
+jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])
